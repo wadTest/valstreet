e3bd36e9f2b0e39a97980ae47336f56b5050064a
wad <wadwatsana@gmail.com>
2019-03-18 14:44:50 +0700

    class message 18/3/62(14.44)

--- app/src/main/java/com/prospec/prospecservice/CustomDialogActivity.java
+++ app/src/main/java/com/prospec/prospecservice/CustomDialogActivity.java
@@ -2,9 +2,11 @@ package com.prospec.prospecservice;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,18 +24,37 @@ public class CustomDialogActivity extends AppCompatActivity {
     private Button buttonMessage;
     private String editTextString;
     private Toolbar toolbar;
+//    share name title
+    private String nameLogin, titleLogin;
+    private TextView tv_name, title;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custom_dialog);
 
+        share();
+        toolBar();
+        getEvent();
+        click();
+
+    }//Method
+
+    private void share() {
+//        share name and title to DB.
+        SharedPreferences sharedPreferences = getSharedPreferences("Logout", MODE_PRIVATE);
+        titleLogin = sharedPreferences.getString("titleLogin", "");
+        nameLogin = sharedPreferences.getString("NameLogin", "");
+        Log.d("share title, name", "get name message" + titleLogin + nameLogin);
+    }//end share
+
+    private void toolBar() {
 //        ADD Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("รายละเอียดสินทรัพย์");
-        toolbar.setSubtitle("โปรดกรอกรายละเอียดสินทรัพย์ที่จะประเมิน พร้อมที่ตั้งทรัพย์สิน");
-       // toolbar.setLogo(R.drawable.logo_prospec);
+        toolbar.setSubtitle("โปรดกรอกรายละเอียดสินทรัพย์");
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -42,29 +63,39 @@ public class CustomDialogActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }//end toolbar
 
+    private void getEvent() {
         editText = (EditText) findViewById(R.id.editText);
         buttonMessage = (Button) findViewById(R.id.buttonMessage);
+//        ชื่อคนที่login
+        tv_name = (TextView) findViewById(R.id.tv_name);
+        tv_name.setText(nameLogin.trim());
+//        title TB :uers_app_crm ไปดูใน MySQL
+        title = (TextView) findViewById(R.id.title);
+        title.setText(titleLogin.trim());
+    }//end event
 
+    private void click() {
         buttonMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 editTextString = editText.getText().toString().trim();
-
+//                  check ความว่างเปล่า
                 if (editTextString.equals("")) {
-
+//                    ถ้าไม่กรอกจะขึ้นแจ้งเตือน
                     MyAlert myAlert = new MyAlert(CustomDialogActivity.this, "มีช่องว่าง", "กรุณากรอกข้อมูลในช่องว่าง");
                     myAlert.myDialog();
 
                 } else {
-//                    upload ข้อมูลที่กรอกไปเก็บไว้ใน my sql
+//                    upload ข้อมูลที่กรอกไปเก็บไว้ใน MySQL
                     uploadString();
                 }
             }//onClick
 
             private void uploadString() {
                 try {
+//                    File เชื่อมต่อ DB. หน้านี้ Add_Message
                     Add_Message add_message = new Add_Message(CustomDialogActivity.this, editTextString);
                     add_message.execute();
 
@@ -75,6 +106,5 @@ public class CustomDialogActivity extends AppCompatActivity {
                 }
             }
         });
-
-    }//Method
+    }
 }//Main Class
